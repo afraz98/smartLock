7bd528ed117f7d4106bbd6a2a996f4eb7e018e10
afraz98 <anthonymfrazier1998@gmail.com>
2020-11-25 08:51:53 -0500

    pre-AWS Cognito implementation

---
+++ app/src/main/java/com/example/safeboxv20/Cognito.java
@@ -0,0 +1,8 @@
+package com.example.safeboxv20;
+import com.amazonaws.mobileconnectors.cognitoauth.Auth;
+
+public class Cognito {
+    private String userPoolId, clientId;
+    public Cognito(){
+    }
+}
--- app/src/main/java/com/example/safeboxv20/Login.java
+++ app/src/main/java/com/example/safeboxv20/Login.java
@@ -1,13 +1,12 @@
 package com.example.safeboxv20;
 import android.app.Activity;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Login extends Activity {
     private Button submit;              //Submit button
@@ -15,8 +14,8 @@ public class Login extends Activity {
     private String useremail = "", userpassword = "";
     private ImageView img;
 
-    public void authenticateUser(){
-        // ...
+    public boolean authenticateUser(String user, String password){
+        return true;
     }
 
     @Override
@@ -29,16 +28,25 @@ public class Login extends Activity {
         email = findViewById(R.id.emailinput);
         password = findViewById(R.id.passwordinput);
         img = findViewById(R.id.loginicon);
-
         img.setImageResource(R.drawable.login_icon);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
+                boolean validUser = false;
                 useremail = email.getText().toString();
                 userpassword = password.getText().toString();
+
+                validUser = authenticateUser(useremail, userpassword);
+                if (validUser) {
+                    img.setImageResource(R.drawable.unlock_icon);
                     startActivity(intent);
+                } else prompt("Invalid password or email. Please try again.");
             }
         });
+    }
+
+    private void prompt(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
