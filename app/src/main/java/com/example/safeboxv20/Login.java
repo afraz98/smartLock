9f3a815a0e44eec2632b5d941f1720ef36d0f81b
afraz98 <anthonymfrazier1998@gmail.com>
2020-11-25 00:29:28 -0500

    Log-in XML Implemented. TO DO: Implement Cognito SDK

--- app/src/main/java/com/example/safeboxv20/Login.java
+++ app/src/main/java/com/example/safeboxv20/Login.java
@@ -1,16 +1,20 @@
 package com.example.safeboxv20;
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class Login extends Activity {
     private Button submit;              //Submit button
     private EditText email, password;   //Email, password parameters
     private String useremail = "", userpassword = "";
+    private ImageView img;
+
     public void authenticateUser(){
         // ...
     }
@@ -24,6 +28,9 @@ public class Login extends Activity {
         submit = findViewById(R.id.submit);
         email = findViewById(R.id.emailinput);
         password = findViewById(R.id.passwordinput);
+        img = findViewById(R.id.loginicon);
+
+        img.setImageResource(R.drawable.login_icon);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
